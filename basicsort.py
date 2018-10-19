da0acae08d01cfce9ea14b1d20881742f59529c2
Alex Kyungho Park <30945486+konkuest@users.noreply.github.com>
2018-10-19 19:27:26 +0900

    #3 Bubblesort

--- basicsort.py
+++ basicsort.py
@@ -2,6 +2,8 @@
 import time
 import random
 
+#http://ejklike.github.io/2017/03/04/sorting-algorithms-with-python.html
+
 def swap(x,i,j):
     x[i], x[j] = x[j], x[i]
     #x : input array
@@ -15,6 +17,12 @@ def selectionSort(x):
         swap(x, max_i, size) #가장 마지막의 값과 교체해줌
     return x
     
+def bubblesort(x):
+    for size in reversed(range(len(x))): #전체 개수 중에서
+        for i in range(size):
+            if x[i] > x[i+1]: #size=1 이면 1번, size=2면 2번, size=3면 3번... 반복
+                swap(x, i, i+1)
+    return x
 
 insert = [3,5,7,6,8,4,1]
 print(selectionSort(insert))
